6aeac2b6b02f904a03fe03ccba2145cd67380b9d
shehanbandara <s2bandara@uwaterloo.ca>
2022-01-05 19:26:46 -0500

    New location displayed after submitting form
    New location displayed after submitting form

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-const Form = () => {
+import { createLogEntry } from '../API';
+
+const Form = ({ location, onClose }) => {
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
     const { register, handleSubmit } = useForm();
 
-    const onSubmit = (data) => {
-        console.log(data);
+    const onSubmit = async (data) => {
+        try {
+            data.latitude = location.latitude;
+            data.longitude = location.longitude;
+            await createLogEntry(data);
+            onClose();
+        } catch (error) {
+            console.error(error);
+            setError(error.message);
+            setLoading(false);
+        }
     };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="form">
+            {error ? <h3 className="error">{error}</h3> : null}
             <label htmlFor="title">📍 Location: </label>
             <input {...register('title', { required: true })} />
             <label htmlFor="comments">💭 Comments: </label>
@@ -18,7 +32,7 @@ const Form = () => {
             <input{...register('image', { required: false })} />
             <label htmlFor="visitDate">📅 Date Visited: </label>
             <input type="date" {...register('visitDate', { required: true })} />
-            <button>🌎 Add Travel Log Entry 🌎</button>
+            <button disabled={loading}>🌎 Add Travel Log Entry 🌎</button>
         </form>
     );
 }
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 
 import Form from './Form';
-import Pin from './Pin';
+import { listLogEntries } from '../API';
 
 const Map = () => {
     const [addPinLocation, setAddPinLocation] = useState(null);
+    const [logEntries, setLogEntries] = useState([]);
+    const [showPopup, setShowPopup] = useState({});
     const [viewport, setViewport] = useState({
         width: '100vw',
         height: '100vh',
@@ -22,6 +24,15 @@ const Map = () => {
         });
     };
 
+    const getEntries = async () => {
+        const logEntries = await listLogEntries();
+        setLogEntries(logEntries);
+    };
+
+    useEffect(() => {
+        getEntries();
+    }, []);
+
     return (
         <ReactMapGL
             {...viewport}
@@ -29,7 +40,56 @@ const Map = () => {
             mapStyle="mapbox://styles/thecjreynolds/ck117fnjy0ff61cnsclwimyay"
             onViewportChange={setViewport}
             onDblClick={AddPin} >
-            <Pin />
+            {
+                logEntries.map(entry => (
+                    <React.Fragment key={entry._id}>
+                        <Marker
+                            key={entry._id}
+                            latitude={entry.latitude}
+                            longitude={entry.longitude} >
+                            <div
+                                onClick={() => setShowPopup({
+                                    [entry._id]: true
+                                })} >
+                                <svg
+                                    className="pin blue"
+                                    style={{
+                                        height: `${6 * viewport.zoom}px`,
+                                        width: `${6 * viewport.zoom}px`,
+                                    }}
+                                    version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
+                                    <g>
+                                        <g>
+                                            <path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
+                                c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
+                                c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"/>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </div>
+                        </Marker>
+                        {
+                            showPopup[entry._id] ? (
+                                <Popup
+                                    latitude={entry.latitude}
+                                    longitude={entry.longitude}
+                                    closeButton={false}
+                                    closeOnClick={true}
+                                    dynamicPosition={true}
+                                    onClose={() => setShowPopup({})}
+                                    anchor="top" >
+                                    <div className="popup">
+                                        <h3>📍 {entry.title}</h3>
+                                        <p>💭 {entry.comments}</p>
+                                        <small>📅 Visited On: {new Date(entry.visitDate).toLocaleDateString()}</small>
+                                        {entry.image && <img src={entry.image} alt={entry.title} />}
+                                    </div>
+                                </Popup>
+                            ) : null
+                        }
+                    </React.Fragment>
+                ))
+            }
             {
                 addPinLocation ? (
                     <>
@@ -63,7 +123,12 @@ const Map = () => {
                             onClose={() => setAddPinLocation(null)}
                             anchor="top" >
                             <div className="popup">
-                                <Form />
+                                <Form
+                                    location={addPinLocation}
+                                    onClose={() => {
+                                        setAddPinLocation(null);
+                                        getEntries();
+                                    }} />
                             </div>
                         </Popup>
                     </>
